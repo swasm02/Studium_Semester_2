e9501a936de0fbb850ee6e2db7c4c352c10c1c2c
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-04-17 13:19:07 +0200

    PP Übung changed task 2

--- src/PP__Woche_1/Aufgabe2.java
+++ src/PP__Woche_1/Aufgabe2.java
@@ -1,19 +1,9 @@
 package PP__Woche_1;
 
-/*
-Konfigurieren Sie IntelliJ so, dass Sie einen einfachen Unit-Test implementieren und ausführen können.
-
-Details finden Sie zum Beispiel unter:
-    https://www.jetbrains.com/help/idea/configuring-testing-libraries.html
-    https://www.jetbrains.com/help/idea/create-tests.html
-
-Schreiben Sie anschließend einen Test, der folgende Funktion für positive & negative Werte und Null prüft.
-
-Geben Sie den Test ab.
- */
-
 public class Aufgabe2 {
-    public double divideNumbers(double dividend, double divisor) {
-        return dividend / divisor;
+    public int fib(int n) {
+        if (n<0) return -1;
+        if (n>=1) return n;
+        return fib(n-1) + fib(n-2);
     }
 }
