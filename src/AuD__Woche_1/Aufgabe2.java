1a2b9c018a338d984d31546cfd430edf03ab3a31
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-04-18 11:28:30 +0200

    AuD Blatt 1 Aufgabe 2

--- src/AuD__Woche_1/Aufgabe2.java
+++ src/AuD__Woche_1/Aufgabe2.java
@@ -1,10 +1,9 @@
 package AuD__Woche_1;
-
 import java.util.Arrays;
 
 public class Aufgabe2 {
     public static void main(String[] args) {
-        int[] sommertage = {99, -9, 5, -1, 12, -101, 100};
+        int[] sommertage = {0, 18, 5, 9, 0};
         System.out.println(Arrays.toString(sommertage));
         System.out.println(Arrays.toString(SummerLin(sommertage)));
     }
@@ -12,7 +11,6 @@ public class Aufgabe2 {
     public static int[] SummerLin(int[] a) {
         int maxsum = Integer.MIN_VALUE;
         int maxendsum = Integer.MIN_VALUE;
-
         int maxendind = -1; int i = -1; int j = -1;
 
         for (int l = 0; l < a.length; l++) {
@@ -23,13 +21,13 @@ public class Aufgabe2 {
                 maxendsum = maxendsum + a[l];
             }
 
-            if (maxsum < maxendsum) {
+            if (maxsum <= maxendsum) {
                 maxsum = maxendsum;
                 i = maxendind;
                 j = l;
             }
         }
-
+        System.out.println("First: " + a[i] + " Last: " + a[j]);
         return new int[] {i, j};
     }
 }
