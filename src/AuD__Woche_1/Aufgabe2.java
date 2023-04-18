bec8deb3426e1db47f1721d0df0a399c547fe806
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-04-18 18:33:21 +0200

    AuD Blatt 1 Aufgabe 2 with further tasks

--- src/AuD__Woche_1/Aufgabe2.java
+++ src/AuD__Woche_1/Aufgabe2.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 
 public class Aufgabe2 {
     public static void main(String[] args) {
-        int[] sommertage = {0, 18, 5, 9, 0};
+        int[] sommertage = {0,9,-20,4,5,-20,9,0};
         System.out.println(Arrays.toString(sommertage));
-        System.out.println(Arrays.toString(SummerLin(sommertage)));
     }
 
     public static int[] SummerLin(int[] a) {
@@ -29,5 +28,47 @@ public class Aufgabe2 {
         }
         System.out.println("First: " + a[i] + " Last: " + a[j]);
         return new int[] {i, j};
+    }
+
+}
+
+class linked_list {
+    private int i;
+    private int j;
+    private linked_list next;
+
+    linked_list(int i, int j) {
+        this.i = i;
+        this.j = j;
+        next = null;
+    }
+    linked_list() {
+        i = -1; j = -1; next = null;
+    }
+    public int getI() {
+        return i;
+    }
+    public void setI(int i) {
+        this.i = i;
+    }
+    public int getJ() {
+        return j;
+    }
+    public void setJ(int j) {
+        this.j = j;
+    }
+    public linked_list getNext() {
+        return next;
+    }
+    public void setNext(linked_list next) {
+        this.next = next;
+    }
+    public String toString() {
+        String s = "i: " + getI() + " j: " + getJ();
+        while (next != null) {
+            s += " | " + "i: " + next.getI() + " j: " + next.getJ();
+            next = getNext();
+        }
+        return s;
     }
 }
