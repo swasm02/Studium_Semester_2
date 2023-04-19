5bc146d6ced5f6427c432c9610e38e7cf94d9b72
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-04-19 16:01:34 +0200

    AuD Blatt 1 Aufgabe 2 with further tasks + deleted linked list

--- src/AuD__Woche_1/Aufgabe2.java
+++ src/AuD__Woche_1/Aufgabe2.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 
 public class Aufgabe2 {
     public static void main(String[] args) {
-        int[] sommertage = {0,9,-20,4,5,-20,9,0};
-        System.out.println(Arrays.toString(sommertage));
+        int[] sommertage = {0,10,-20,11,-20,9,0};
+        System.out.println("zu überprüfendes Jahr: " + Arrays.toString(sommertage));
+        System.out.println("Sommerperiode: " + Arrays.toString(SummerLin(sommertage)));
+        System.out.println("Südsommerperiode: " + Arrays.toString(south_summer_period(sommertage)));
     }
 
     public static int[] SummerLin(int[] a) {
@@ -26,49 +28,31 @@ public class Aufgabe2 {
                 j = l;
             }
         }
-        System.out.println("First: " + a[i] + " Last: " + a[j]);
+        //System.out.println("First: " + a[i] + " Last: " + a[j]);
         return new int[] {i, j};
     }
 
-}
+    public static int[] south_summer_period(int[] a) {
+        int maxsum = Integer.MIN_VALUE;
+        int maxendsum = Integer.MIN_VALUE;
+        int maxendind = -1; int i = -1; int j = -1;
 
-class linked_list {
-    private int i;
-    private int j;
-    private linked_list next;
+        for (int l = 0; l < 2 * a.length; l++) {
+            int c = l % a.length;
+            if (a[c] > maxendsum + a[c]) {
+                maxendind = c;
+                maxendsum = a[c];
+            } else {
+                maxendsum = maxendsum + a[c];
+            }
 
-    linked_list(int i, int j) {
-        this.i = i;
-        this.j = j;
-        next = null;
-    }
-    linked_list() {
-        i = -1; j = -1; next = null;
-    }
-    public int getI() {
-        return i;
-    }
-    public void setI(int i) {
-        this.i = i;
-    }
-    public int getJ() {
-        return j;
-    }
-    public void setJ(int j) {
-        this.j = j;
-    }
-    public linked_list getNext() {
-        return next;
-    }
-    public void setNext(linked_list next) {
-        this.next = next;
-    }
-    public String toString() {
-        String s = "i: " + getI() + " j: " + getJ();
-        while (next != null) {
-            s += " | " + "i: " + next.getI() + " j: " + next.getJ();
-            next = getNext();
+            if (maxsum <= maxendsum) {
+                maxsum = maxendsum;
+                i = c;
+                j = maxendind;
+            }
         }
-        return s;
+        //System.out.println("First: " + a[j] + " Last: " + a[i]);
+        return new int[] {j, i};
     }
 }
